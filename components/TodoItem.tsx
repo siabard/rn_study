3e9e77a8b7cadc251063a9d7dcb0c7ba7824529b
Yeonho Jang <siabard@gmail.com>
2022-07-18 16:04:04 +0900

    Add Icon (Vector)

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const styles = StyleSheet.create({
-    item: {
-        padding: 16,
-        marginTop: 16,
-        borderColor: '#bbb',
-        borderWidth: 1,
-        borderStyle: 'dashed',
-        borderRadius: 10
-    }
+  item: {
+    flexDirection: 'row',
+    padding: 16,
+    marginTop: 16,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 10,
+  },
+  itemText: {
+    marginLeft: 10,
+  },
 });
 
-const TodoItem = ({item, pressHandler} : { item: { text: string, key: string }, pressHandler: (v: string) => void}) => {
-    return (
-        <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
-        </TouchableOpacity>
-    )
+const TodoItem = ({
+  item,
+  pressHandler,
+}: {
+  item: {text: string; key: string};
+  pressHandler: (v: string) => void;
+}) => {
+  return (
+    <TouchableOpacity onPress={() => pressHandler(item.key)}>
+      <View style={styles.item}>
+        <Icon name="delete" size={18} color="#333" />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 export default TodoItem;
